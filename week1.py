308636af9928a3d3f186b20ee69c0b1701fba577
Sahana-pallavi <sahanabc9781@gmail.com>
2020-08-14 20:29:33 +0530

    some descriptive message for the commit

---
+++ week1.py
@@ -0,0 +1,19 @@
+TYPES:
+    how python represents different type of data.
+Text Type:	str
+Numeric Types:	int, float, complex
+Sequence Types:	list, tuple, range
+Mapping Type:	dict
+Set Types:	set, frozenset
+Boolean Type:	bool
+Binary Types:	bytes, bytearray, memoryview
+EX: type(11)= int
+    type(21.21)= float
+	type("Hello")= str
+TYPECASTING
+    str(1):"1"
+	str(4.5): '4.5'
+   Boolean  :  int(True)=1
+               int(False)=0
+               bool(1)=True
+               bool(0)= False			  
